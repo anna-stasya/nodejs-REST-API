0181a08c4e62b802dd0b7789f752e7b3605e3eae
Anastasiia Sanatar <sanatar.nastya@gmail.com>
2021-11-23 23:54:02 +0200

    почистила код + добавила  проверку

--- controllers/auth/signup.js
+++ controllers/auth/signup.js
@@ -1,4 +1,3 @@
-// const bcrypt = require('bcryptjs')
 const { Conflict } = require('http-errors')
 const { User } = require('../../model/Schemas/user')
 
@@ -7,19 +6,11 @@ const signup = async (req, res) => {
   const user = await User.findOne({ email })
   if (user) {
     throw new Conflict(`Email ${email} in use`)
-    // res.status(409).json({
-    //   status: 'conflict',
-    //   code: 409,
-    //   message: `Email ${email} in use`
-    // })
-    // return
   }
   const newUser = new User({ email })
   newUser.setPassword(password)
   await newUser.save()
-  // const salt = bcrypt.genSaltSync(10)
-  // const hashPassword = bcrypt.hashSync(password, salt)
-  // const newUser = await User.create({ email, password: hashPassword })
+
   res.status(201).json({
     status: 'success',
     code: 201,
--- middelevwares/authenticate.js
+++ middelevwares/authenticate.js
@@ -1,11 +1,16 @@
 const { Unauthorized, NotFound } = require('http-errors')
 const jwt = require('jsonwebtoken')
+require('dotenv').config()
 
 const { User } = require('../model/Schemas/contact')
 const { SECRET_KEY } = process.env
 
 const authenticate = async (req, res, next) => {
   try {
+    if (!req.headers.authorization) {
+      throw new Unauthorized()
+    }
+
     const [bearer, token] = req.headers.authorization.split(' ')
 
     if (bearer !== 'Bearer') {
